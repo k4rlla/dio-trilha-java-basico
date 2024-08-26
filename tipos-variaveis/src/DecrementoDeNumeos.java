d73dab20da616e398e8f16b4ba51cc5d4eb78356
karla <karllinha.sousa@gmail.com>
2024-08-26 16:30:55 -0300

    decremento de numeros

---
+++ tipos-variaveis/src/DecrementoDeNumeos.java
@@ -0,0 +1,15 @@
+public class DecrementoDeNumeos {
+    public static void main(String[] args) {
+            int numero = 5;
+            //x repeticao
+           
+            // numero--;
+            //numero = numero - 1; 
+            //System.out.println(numero);
+
+            System.err.println(numero --);
+
+            System.err.println(numero);
+        }
+    
+}
