5ff7bcb0f6375f38d29c874c5d62e41d3f517fcb
karla <karllinha.sousa@gmail.com>
2024-08-26 15:38:50 -0300

    concatenação

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -15,8 +15,6 @@ public class Operadores {
 
         System.out.println(concatenacao);
 
-        System.out.println(concatenacao);
-
         concatenacao = "1"+(1+1+1);
 
         System.out.println(concatenacao);
