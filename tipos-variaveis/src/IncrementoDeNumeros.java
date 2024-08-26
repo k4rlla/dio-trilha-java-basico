09eb256f8e92e86c12ee015725fa53ff8759d2cc
karla <karllinha.sousa@gmail.com>
2024-08-26 16:25:53 -0300

    incremento de numeros

---
+++ tipos-variaveis/src/IncrementoDeNumeros.java
@@ -0,0 +1,11 @@
+public class IncrementoDeNumeros {
+    public static void main(String[] args) {
+        int numero = 5;
+        //x repeticao
+       
+        numero++;
+         //numero = numero + 1; 
+
+        System.out.println(numero);
+    }
+}
