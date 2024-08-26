21543027fdc3b704cfb21c6b0c0cfbf0f3e61ac6
karla <karllinha.sousa@gmail.com>
2024-08-26 15:35:07 -0300

    concatenaçoes

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,10 +1,5 @@
 public class Operadores {
-    
-    public static void main(String[] args) {
-        String nomeCompleto = "KARLLA" + "OLIVEIRA";
-
-        System.out.println(nomeCompleto);
-    }
+     
     public static void main(String[] args) {
         String concatenacao = "?";
 
