21ac63e35eefd95bdfbf8a88c8186b5d4b991bd8
karla <karllinha.sousa@gmail.com>
2024-08-29 14:57:39 -0300

    operador logico

---
+++ tipos-variaveis/src/OperadoresLogicos.java
@@ -0,0 +1,15 @@
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+        boolean condicao1 = true;
+
+        boolean condição2 = true;
+
+        if(condicao1 && condição2){
+            System.out.println("As duas condições são veradadeiras");
+        }
+        if(condicao1 || condição2){
+            System.out.println("Uma das condições é verdadeira");
+        }
+        System.out.println("Fim");
+    }
+}
