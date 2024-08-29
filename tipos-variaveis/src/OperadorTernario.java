10650a36fe6a5881c0bb636ec2e5af28b2b0cb7e
karla <karllinha.sousa@gmail.com>
2024-08-29 11:03:18 -0300

    operador ternario

---
+++ tipos-variaveis/src/OperadorTernario.java
@@ -0,0 +1,20 @@
+public class OperadorTernario {
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 6;
+        b = 6;
+
+        // String resultado = "";
+        // if(a==b)
+        //     resultado = "verdadeiro";
+        // else
+        //     resultado = "falso";
+
+        // usando o Operador Ternário:
+
+        String resultado = a==b ? "verdadeiro" : "falso";
+        
+        System.out.println(resultado);
+    }
+}
