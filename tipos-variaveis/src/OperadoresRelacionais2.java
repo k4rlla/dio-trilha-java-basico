32f71ee25e2815c550cdd3066d356e0d4d9b6506
karla <karllinha.sousa@gmail.com>
2024-08-29 11:41:46 -0300

    objetos

---
+++ tipos-variaveis/src/OperadoresRelacionais2.java
@@ -0,0 +1,13 @@
+public class OperadoresRelacionais2 {
+    public static void main(String[] args) {
+        String nomeUm = "KARLLA";
+        String nomeDois = new String("KARLLA");
+
+        System.out.println(nomeUm.equals(nomeDois));
+        // comparando dois objetos ou textos usa-se outra metódo chamado equals e não os operadores relacionais == que são usados para números!
+
+        // String nomeUm = "KARLLA";
+        // String nomeDois = "KARLLA";
+        // System.out.println(nomeUm == nomeDois);
+    }
+}
