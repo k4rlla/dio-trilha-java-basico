e59bbaea5c893b5abe5691c757467d5e7d8b21f0
karla <karllinha.sousa@gmail.com>
2024-08-29 15:05:35 -0300

    valores booleanos e valores relacionais

--- tipos-variaveis/src/OperadoresLogicos.java
+++ tipos-variaveis/src/OperadoresLogicos.java
@@ -1,10 +1,22 @@
 public class OperadoresLogicos {
     public static void main(String[] args) {
+        // boolean condicao1 = true;
+
+        // boolean condição2 = true;
+
+        // if(condicao1 && condição2){
+        //     System.out.println("As duas condições são veradadeiras");
+        // }
+        // if(condicao1 || condição2){
+        //     System.out.println("Uma das condições é verdadeira");
+        // }
+        // System.out.println("Fim");
+
         boolean condicao1 = true;
 
         boolean condição2 = true;
 
-        if(condicao1 && condição2){
+        if(condicao1 && (7 > 4 )){
             System.out.println("As duas condições são veradadeiras");
         }
         if(condicao1 || condição2){
