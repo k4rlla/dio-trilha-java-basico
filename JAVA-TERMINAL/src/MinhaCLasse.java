23699865711cb8e81507be4cac12f2548c9cb771
karla <karllinha.sousa@gmail.com>
2024-09-04 16:33:30 -0300

    terminal

--- JAVA-TERMINAL/src/MinhaCLasse.java
+++ JAVA-TERMINAL/src/MinhaCLasse.java
@@ -1,6 +1,6 @@
 public class MinhaCLasse {
    public static void main(String[] args) {
-       System.out.println("Oi Javeiros, fui executado pelo Terminal");
+       System.out.println("Oi Javeiros. Que emoção!! Fui executado pelo Terminal");
    } 
-   
+
 }
